9043f368b14d17359cb61e24317f3b22d7ea1c14
Raúl Martín <626raul626@gmail.com>
2025-01-26 17:23:26 +0100

    bubble horizontal movement

--- bubble.gd
+++ bubble.gd
@@ -31,31 +31,35 @@ func tick_process():
 
 	# if hazard collision wait for animation to pop
 	var collisionDetected = false
-	var shouldPop = false
 	
 	for buildingPlacement in canvas.placements:
 		var collisionRes = buildingPlacement.get_collision(canvasPos, moveDir)
 		if collisionRes == buildingPlacement.CollisionType.Pop:			
-			shouldPop = true
-			break
+			if moveDir.x != 0:
+				move_bubble_horizontal(moveDir.x)
+			else:
+				move_bubble_vertical(moveDir.y)
+			pop(simulationStepTimeMs/3.0)		
+			return
 		elif collisionRes == buildingPlacement.CollisionType.Block:
 			collisionDetected = true
 		
-	if not collisionDetected or shouldPop:
+	if not collisionDetected or moveDir.x != 0:
 		if moveDir.x != 0:
-			move_bubble_horizontal(moveDir.x)
+			if collisionDetected:
+				move_bubble_vertical(moveDir.y)
+			else:
+				move_bubble_horizontal(moveDir.x)
 		else:
-			move_bubble_vertical(moveDir.y)
-		if shouldPop:
-			pop(simulationStepTimeMs/3.0)	
-		else:
-			movedIntoNewPos.emit(canvasPos)
+			move_bubble_vertical(moveDir.y)			
+		movedIntoNewPos.emit(canvasPos)
 
 
 
 func set_position_in_canvas(pos: Vector2i):
 	canvasPos = pos
 	self.position = Vector3(pos.x * canvas.tileSize, pos.y * canvas.tileSize, canvas.tileSize/8)
+
 
 # Calculates where bubble should move given a position
 func get_moving_dir():
@@ -69,21 +73,36 @@ func get_moving_dir():
 				hFans.append(buildingPlacement)
 			else:
 				vFans.append(buildingPlacement)
+				
+			
 			
 	if fans.is_empty():
 		return Vector2i(0,1) 
 		
 	var moveDir = Vector2i(0,0)
 	
+	var leftStrength = 0
+	var rightStrength = 0
+	
 	for fan: Canvas.BuildingPlacement in hFans:
 		if fan.position.y == canvasPos.y:
 			if fan.orientation == Building.Orientation.LEFT:
 				if fan.position.x < canvasPos.x and canvasPos.x - fan.position.x <= 3:
-					pop(simulationStepTimeMs)
+					var strength = 4 - (canvasPos.x - fan.position.x)
+					if leftStrength < strength:
+						leftStrength = strength
 			elif fan.orientation == Building.Orientation.RIGHT:
 				if fan.position.x > canvasPos.x and fan.position.x - canvasPos.x <= 3:
-					pop(simulationStepTimeMs)
-		pass
+					var strength = 4 - (fan.position.x - canvasPos.x)
+					if rightStrength < strength:
+						rightStrength = strength
+	
+	 		
+	if leftStrength != rightStrength:
+		if leftStrength > rightStrength:
+			return Vector2i(1, 0)
+		else:
+			return Vector2i(-1, 0)
 		
 	return Vector2i(0,1) 
 
