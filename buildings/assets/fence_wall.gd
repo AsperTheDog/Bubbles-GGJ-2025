293f2974e537655d645b3bc2a1c714e8be5d40e2
AsperTheDog <guillerman0000@gmail.com>
2025-01-26 01:02:58 +0100

    Mini fix and side panel data

--- buildings/assets/fence_wall.gd
+++ buildings/assets/fence_wall.gd
@@ -3,14 +3,3 @@ extends BaseBuilding
 
 func getMeshes():
 	return [$plate]
-
-
-func orient(orientation: BaseBuilding.Orientation):
-	direction = orientation
-	match orientation:
-		Orientation.LEFT:
-			rotate(Vector3.FORWARD, deg_to_rad(-90))
-		Orientation.RIGHT:
-			rotate(Vector3.FORWARD, deg_to_rad(90))
-		Orientation.BOTTOM:
-			rotate(Vector3.FORWARD, deg_to_rad(180))
